ae8d9d20d0f90c2f65b3c8b0991b29516babf121
GiovanniConti <giovanni.conti@live.it>
2021-12-21 19:15:44 +0100

    Added and linked comic detail page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,14 +12,16 @@
       {{-- Ciclo foreach per stampare tutte le card --}}
       @foreach ($comics_list as $comic)
       <div class="col">
-        <div class="card card-comic">
+        <a class="card card-comic"
+          href="{{ route('products.comic_page', ["id" => $comic['id']]) }}"
+        >
           <div class="card-img-container">
             <img class="card-img-top" src="{{ $comic['thumb']}}" alt="">
           </div>
           <div class="card-body">
             <h5 class="card-title">{{ $comic['series']}}</h5>
           </div>
-        </div>
+        </a>
       </div>
       @endforeach
 
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   $comis_data = config("db_comics");
+  $final_comics_data = [];
+
+  foreach ($comis_data as $index => $comic){
+    $comic['id'] = $index;
+    $final_comics_data[] = $comic;
+  }
+  dump($final_comics_data);
+
   return view('home.index', [
-    "comics_list" => $comis_data
+    "comics_list" => $final_comics_data
   ]);
 })->name("home");
+
+Route::get('/comics', function() {
+  return view('products.comic_page');
+})->name('products.comic_page');
